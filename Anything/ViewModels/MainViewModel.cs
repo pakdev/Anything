6b315a61c1c27777ebca7c4578c962c53089bf96
Peter Kurlak <pkurlak@gmail.com>
2016-03-15 23:29:59 -0500

    Initial commit

---
+++ Anything/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using GalaSoft.MvvmLight;
+
+namespace Anything.ViewModels
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private string _search;
+        public string Search
+        {
+            get { return _search; }
+            set
+            {
+                Set(ref _search, value);
+            }
+        }
+    }
+}
